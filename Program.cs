2e0c0cd61cedd5a13d7fccf781239ac8f4e9887d
rohacsidana <rohacsi.dana@gmail.com>
2023-10-10 09:14:19 +0200

    első verzió

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Beadando
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+
+            int oneCounter = 0;
+
+            foreach (var c in binary)
+            {
+                if (c == '1')
+                {
+                    oneCounter++;
+                }
+            }
+
+            Console.WriteLine(oneCounter);
+        }
+    }
+}
